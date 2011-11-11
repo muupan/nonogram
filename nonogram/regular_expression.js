932a691a615fab0753140d3dee1af6f82d444ae5
Yasuhiro Fujita <muupan@gmail.com>
2011-11-11 17:06:08 +0900

    簡易化

--- nonogram/regular_expression.js
+++ nonogram/regular_expression.js
@@ -6,7 +6,7 @@ function escape() {
 
 function unescape() {
 	var before = $("#escaped").val();
-	var after = before.replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/(\\\\|\\\*|\\\+|\\\.|\\\?|\\\{|\\\}|\\\(|\\\)|\\\[|\\\]|\\\^|\\\$|\\\-|\\\||\\\/)/g, function (whole, s1) {
+	var after = before.replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/(\\(\\|\*|\+|\.|\?|\{|\}|\(|\)|\[|\]|\^|\$|\-|\||\/))/g, function (whole, s1) {
 		return s1.substring(1);
 	});
 	$("#notEscaped").val(after);
